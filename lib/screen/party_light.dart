53ef1aa69ab42932c51465db8c98fb6203094b12
Yun-sooyong <wood5186@naver.com>
2022-09-06 03:49:00 +0900

    병 조명을 위한 컬러 그라데이션

--- lib/screen/party_light.dart
+++ lib/screen/party_light.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:animate_gradient/animate_gradient.dart';
 
 class PartyLight extends StatefulWidget {
-  const PartyLight({Key? key}) : super(key: key);
+  const PartyLight({super.key});
 
   @override
   State<PartyLight> createState() => _PartyLightState();
@@ -10,11 +13,35 @@ class PartyLight extends StatefulWidget {
 class _PartyLightState extends State<PartyLight> {
   @override
   Widget build(BuildContext context) {
-    //Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('Party light'),
+      body: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: size.height * 0.44,
+            width: size.width,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(300),
+              child: AnimateGradient(
+                // 컬러 부위를 랜덤으로 처리
+                primaryColors: const [
+                  Colors.red,
+                  Colors.orange,
+                ],
+                secondaryColors: const [
+                  Colors.blue,
+                  Colors.cyan,
+                ],
+                child: const Center(
+                  child: Text('Gradient'),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
